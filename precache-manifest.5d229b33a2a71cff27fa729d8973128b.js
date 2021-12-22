a02baa5416c7c3ab7d13df9dc6055e3538520eae
farhankhangit <farhanabbas.niazi@gmail.com>
2021-12-22 15:09:13 +0500

    Updates

---
+++ precache-manifest.5d229b33a2a71cff27fa729d8973128b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507b9ad9e56f8f57a13925d2d10c3bb7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "94fd8300d94d5bee88c5",
+    "url": "/monsters-rolodex/static/css/main.e0b16ef7.chunk.css"
+  },
+  {
+    "revision": "86b5664a3651e438654c",
+    "url": "/monsters-rolodex/static/js/2.25a570ff.chunk.js"
+  },
+  {
+    "revision": "94fd8300d94d5bee88c5",
+    "url": "/monsters-rolodex/static/js/main.e62dbb50.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "a9b0546c1405ecbd299e97856b36027d",
+    "url": "/monsters-rolodex/static/media/logos.a9b0546c.svg"
+  }
+]);
